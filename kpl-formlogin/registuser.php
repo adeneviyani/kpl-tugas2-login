0c58a8eab9eeb958b26c823f00db802ccf4633ab
adeneviyani <56223358+adeneviyani@users.noreply.github.com>
2021-09-30 10:25:46 +0700

    Delete kpl-formlogin directory

--- kpl-formlogin/registuser.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "koneksi.php";
-$email=$_POST['emailbaru'];
-$password=$_POST['passwordbaru'];
-$perintah="INSERT INTO t_user VALUES ('','" . addslashes($email) . "','" . addslashes($password) . "') ";
-$hasil=$koneksi->query($perintah);
-echo "user berhasil di tambahkan";
